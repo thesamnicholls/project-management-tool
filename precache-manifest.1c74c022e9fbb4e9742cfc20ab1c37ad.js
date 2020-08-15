af61c07fd2bdb986155bffc8f151ecfdf041d42d
Sam Nicholls <sam.nicholls@drpgroup.com>
2020-08-15 21:23:51 +0100

    Updates

---
+++ precache-manifest.1c74c022e9fbb4e9742cfc20ab1c37ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b709371e437a94d3fd9c1fb99b0d53e3",
+    "url": "/project-management-tool/index.html"
+  },
+  {
+    "revision": "cffb9483b6f73fb433a4",
+    "url": "/project-management-tool/static/css/main.92e59715.chunk.css"
+  },
+  {
+    "revision": "d25af51a8d9dba5d4ce2",
+    "url": "/project-management-tool/static/js/2.5133e04b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/project-management-tool/static/js/2.5133e04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cffb9483b6f73fb433a4",
+    "url": "/project-management-tool/static/js/main.2e093aaf.chunk.js"
+  },
+  {
+    "revision": "02ab9cb2b1b1e0129cd5",
+    "url": "/project-management-tool/static/js/runtime-main.9c0d6d8f.js"
+  }
+]);
